af66a48ce074c7d6e34cb152fff0dba9ebec4ee8
nickurt <nickurt@users.noreply.github.com>
2025-02-28 10:01:24 +0100

    wip

--- tests/Providers/nl_NL/PostcodeNLTest.php
+++ tests/Providers/nl_NL/PostcodeNLTest.php
@@ -30,7 +30,7 @@ class PostcodeNLTest extends TestCase
 
     public function test_it_can_get_the_correct_values_for_find_by_postcode_and_house_number_a_valid_postal_code()
     {
-        Http::fake(['https://json.api-postcode.nl?postcode=1118CP&number=202' => Http::response('{"street":"Evert van de Beekstraat","houseNumber":202,"houseNumberAddition":"","postcode":"1118CP","city":"Schiphol","municipality":"Haarlemmermeer","province":"Noord-Holland","rdX":111396,"rdY":479739,"latitude":52.30389933,"longitude":4.74791023,"bagNumberDesignationId":"0394200001001951","bagAddressableObjectId":"0394010001001991","addressType":"building","purposes":["office"],"surfaceArea":16800,"houseNumberAdditions":[""]}')]);
+        Http::fake(['https://api.postcode.nl/rest/addresses/1118CP/202' => Http::response('{"street":"Evert van de Beekstraat","houseNumber":202,"houseNumberAddition":"","postcode":"1118CP","city":"Schiphol","municipality":"Haarlemmermeer","province":"Noord-Holland","rdX":111396,"rdY":479739,"latitude":52.30389933,"longitude":4.74791023,"bagNumberDesignationId":"0394200001001951","bagAddressableObjectId":"0394010001001991","addressType":"building","purposes":["office"],"surfaceArea":16800,"houseNumberAdditions":[""]}')]);
 
         $address = $this->postcodeNL->findByPostcodeAndHouseNumber('1118CP', '202');
 
@@ -53,7 +53,7 @@ class PostcodeNLTest extends TestCase
 
     public function test_it_can_get_the_correct_values_for_find_by_postcode_and_house_number_an_invalid_postal_code()
     {
-        Http::fake(['https://json.api-postcode.nl?postcode=1118CP&number=1' => fn () => throw new HttpClientException('{"exception":"Combination does not exist.","exceptionId":"PostcodeNl_Service_PostcodeAddress_AddressNotFoundException"}', 404)]);
+        Http::fake(['https://api.postcode.nl/rest/addresses/1118CP/1' => fn () => throw new HttpClientException('{"exception":"Combination does not exist.","exceptionId":"PostcodeNl_Service_PostcodeAddress_AddressNotFoundException"}', 404)]);
 
         // GuzzleHttp\Exception\ClientException: Client error: `GET https://json.api-postcode.nl?postcode=1118CP&number=1` resulted in a `404 Not Found` response:
         // {"error":"Cannot resolve address for postcode: 1118CP"}
